52138134e7d51a1be17d1a5e431edd54a4fe5b63
Gautham <korrapatigautham@gmail.com>
2025-08-05 20:58:38 -0700

    dataset download stuff

--- data/gene_expression.py
+++ data/gene_expression.py
@@ -7,4 +7,10 @@ load_dotenv()
 
 syn = synapseclient.Synapse() 
 syn.login(authToken=os.getenv("SYNAPSE_API_KEY")) 
-files = synapseutils.syncFromSynapse(syn, 'syn23650894') 
+
+# loading in datasets
+gene_expression = synapseutils.syncFromSynapse(syn, 'syn23650894') 
+rosmap_metadata = synapseutils.syncFromSynapse(syn, '21073536')
+rosmap_clinical = synapseutils.syncFromSynapse(syn, '3191087')
+imaging_immunofluorescense = synapseutils.syncFromSynapse(syn, '5477198')
+rosmap_rna_mapping = synapseutils.syncFromSynapse(syn, '34572333')
